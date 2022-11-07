1c7223246d810ddbcf679df0ec324bd3af68ddcf
DmitriyBurtsev785 <106624736+DmitriyBurtsev785@users.noreply.github.com>
2022-11-07 22:54:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from additional import create_tables, Publisher, Book
 
 
-DSN = "postgresql://postgres:Lvbnhbq1985@localhost:5432/bookshops"
+DSN = "postgresql://postgres:пароль@localhost:5432/bookshops"
 engine = sqlalchemy.create_engine(DSN)
 
 create_tables(engine)
